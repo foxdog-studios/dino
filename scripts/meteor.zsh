67db3fb045a85505a3d427fc91621b013b6662ea
Peter Sutton <foxxy@foxdogstudios.com>
2014-11-25 13:13:49 +0000

    Formatting

--- scripts/meteor.zsh
+++ scripts/meteor.zsh
@@ -6,15 +6,15 @@ setopt NO_UNSET
 repo=$(realpath "$(dirname -- "$(realpath $0)")/..")
 
 if (( $+METEOR_SETTINGS )); then
-    METEOR_SETTINGS=$(realpath -- $METEOR_SETTINGS)
+  METEOR_SETTINGS=$(realpath -- $METEOR_SETTINGS)
 else
-    METEOR_SETTINGS=$repo/config/default/meteor_settings.json
+  METEOR_SETTINGS=$repo/config/default/meteor_settings.json
 fi
 
 if [[ $# -eq 0 ]]; then
-    args=( --settings $METEOR_SETTINGS )
+  args=( --settings $METEOR_SETTINGS )
 else
-    args=( $@ )
+  args=( $@ )
 fi
 
 cd $repo/dino
